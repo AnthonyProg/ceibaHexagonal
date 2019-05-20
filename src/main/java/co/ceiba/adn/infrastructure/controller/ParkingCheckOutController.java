e7977daf91ed81a916966d277ad970a14f0a5eae
Anthony Luis Hernández Lagos - Ceiba Software <anthony.hernandez@ceiba.com.co>
2019-05-20 08:24:50 -0500

    correcciones

--- src/main/java/co/ceiba/adn/infrastructure/controller/ParkingCheckOutController.java
+++ src/main/java/co/ceiba/adn/infrastructure/controller/ParkingCheckOutController.java
@@ -1,8 +1,5 @@
 package co.ceiba.adn.infrastructure.controller;
 
-import java.time.LocalDateTime;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
